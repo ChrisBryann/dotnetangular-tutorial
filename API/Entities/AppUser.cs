55fb0002097d9e259d4637d2a2acdf9af854efeb
Christopher Bryan <cbryan@ff-inc.com>
2024-02-02 09:03:46 -0800

    initial commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Entities;
+
+public class AppUser
+{
+    // Id is automatically our PK, but if you use another name, add [Key] on top of the definition
+    public int Id { get; set; }
+
+    public string? UserName { get; set; } // this is an optional property
+}
